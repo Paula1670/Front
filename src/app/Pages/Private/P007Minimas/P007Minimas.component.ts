7ff4f694ac83fe85007b93e224ced5f0e2c2e071
Paula1670 <paupulpeg@gmail.com>
2024-07-31 13:53:11 +0200

    MinimasByCampeonatosDONEE

--- src/app/Pages/Private/P007Minimas/P007Minimas.component.ts
+++ src/app/Pages/Private/P007Minimas/P007Minimas.component.ts
@@ -39,7 +39,12 @@ import { FiltrosMinimaDto } from '../../../Models/Private/DtosP007/filtros_minim
   styleUrls: ['./P007Minimas.component.scss'],
 })
 export class P007MinimasComponent implements OnInit {
-  minimalist: P007Minima[] = [];
+  regionalList: P007Minima[] = [];
+  nacionalList: P007Minima[] = [];
+  europeolist: P007Minima[] = [];
+  mundiallist: P007Minima[] = [];
+  olimpicolist: P007Minima[] = [];
+
   authState$: Observable<AuthState>;
   categoriaList: CategoriaEnum[] = [];
   campeonatoList: CampeonatoEnum[] = [];
@@ -59,20 +64,15 @@ export class P007MinimasComponent implements OnInit {
     this.authState$.subscribe({
       next: (data) => {
         this.esEntrenador = data.user?.entrenador;
-        if (this.esEntrenador) this.Get_Minimas();
-        else if (data.user?.idUsuario !== undefined) {
+        if (this.esEntrenador) {
+          this.GetMinimas();
+        } else if (data.user?.idUsuario !== undefined) {
           this.GetGeneroCategoriaByIDUser(data.user?.idUsuario).subscribe(
             (data: P007GetGeneroCategoriaDto) => {
-              console.log(data.Genero);
-              console.log(data.IDCategoria);
-              console.log(data.Categoria);
               this.genero = data.Genero;
               this.categoria = data.IDCategoria;
               this.categoriaNombre = data.Categoria;
-              this.Get_MinimasByFilters({
-                genero: this.genero,
-                categoria: this.categoria,
-              });
+              this.GetMinimas();
             }
           );
         } else {
@@ -81,16 +81,77 @@ export class P007MinimasComponent implements OnInit {
       },
     });
   }
-
-  Get_Minimas() {
-    this.service.Get_Minimas().subscribe((data: any) => {
-      this.minimalist = data;
-    });
+  GetMinimas() {
+    this.Get_MinimasEuropeas();
+    this.Get_MinimasRegionales();
+    this.Get_MinimasMundiales();
+    this.Get_MinimasNacionales();
+    this.Get_MinimasOlimpicas();
+  }
+  Get_MinimasEuropeas() {
+    this.service
+      .Get_MinimasByFilters({
+        campeonato: CampeonatoEnum.Continental,
+        genero: this.genero,
+        categoria: this.categoria,
+      })
+      .subscribe((data: any) => {
+        this.europeolist = data;
+      });
   }
 
+  Get_MinimasRegionales() {
+    this.service
+      .Get_MinimasByFilters({
+        campeonato: CampeonatoEnum.Regional,
+        genero: this.genero,
+        categoria: this.categoria,
+      })
+      .subscribe((data: any) => {
+        this.regionalList = data;
+      });
+  }
+
+  Get_MinimasNacionales() {
+    this.service
+      .Get_MinimasByFilters({
+        campeonato: CampeonatoEnum.Nacional,
+        genero: this.genero,
+        categoria: this.categoria,
+      })
+      .subscribe((data: any) => {
+        this.nacionalList = data;
+      });
+  }
+
+  Get_MinimasMundiales() {
+    this.service
+      .Get_MinimasByFilters({
+        campeonato: CampeonatoEnum.Mundial,
+        genero: this.genero,
+        categoria: this.categoria,
+      })
+      .subscribe((data: any) => {
+        this.mundiallist = data;
+      });
+  }
+
+  Get_MinimasOlimpicas() {
+    this.service
+      .Get_MinimasByFilters({
+        campeonato: CampeonatoEnum.Olimpico,
+        genero: this.genero,
+        categoria: this.categoria,
+      })
+      .subscribe((data: any) => {
+        this.olimpicolist = data;
+      });
+  }
+
+  /*
   Get_MinimasByAno() {
     this.service.Get_MinimasByAno(2024).subscribe((data: any) => {
-      this.minimalist = data;
+      this.europeolist = data;
     });
   }
 
@@ -98,7 +159,7 @@ export class P007MinimasComponent implements OnInit {
     this.service.Get_MinimasByCategoria(1).subscribe((data: any) => {
       this.categoriaList = data;
     });
-  }
+  }*/
 
   gotoFormulario() {
     this.router.navigate(['/add_edit_minima']);
@@ -108,7 +169,7 @@ export class P007MinimasComponent implements OnInit {
     this.service
       .Get_MinimasByFilters(filtros)
       .subscribe((data: P007Minima[]) => {
-        this.minimalist = data;
+        this.europeolist = data;
       });
   }
 
