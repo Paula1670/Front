c368787a630e6d4237e6ac88b3c87033b288a7fa
Paula1670 <paupulpeg@gmail.com>
2024-08-06 14:45:19 +0200

    back

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // UrlBackend: 'https://nestapp-ea9cec2c16b6.herokuapp.com',
-  UrlBackend: 'http://localhost:3000',
+  UrlBackend: 'https://nestapp-ea9cec2c16b6.herokuapp.com',
+  //UrlBackend: 'http://localhost:3000',
 };
